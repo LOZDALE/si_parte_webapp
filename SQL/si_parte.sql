d757594334cf5c8fed33bd6b3043b06cee2e23db
Francesco D'Alessandro <dalefra0@gmail.com>
2025-12-06 19:04:38 +0100

    Aggiunto file SQL del database per setup rapido

---
+++ SQL/si_parte.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Dic 06, 2025 alle 18:51
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `si_parte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `base_budget` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `destinations`
+--
+
+INSERT INTO `destinations` (`id`, `name`, `category`, `base_budget`) VALUES
+(1, 'Roma', 'cultura', 800.00),
+(2, 'Barcellona', 'mare', 1000.00),
+(3, 'Alpi', 'montagna', 1200.00),
+(4, 'Parigi', 'città', 900.00);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `answers` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `answers`) VALUES
+(3, 'Mario Rossi', 'mario@example.com', '{\"climate\":\"mare\",\"budget\":600}'),
+(4, 'Mario Rossi', 'mario@example.com', '{\"climate\":\"mare\",\"budget\":600}'),
+(5, 'Mario Rossi', 'mario@example.com', '{\"climate\":\"mare\",\"budget\":600}');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `destinations`
+--
+ALTER TABLE `destinations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
