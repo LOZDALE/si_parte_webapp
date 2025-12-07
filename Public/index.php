6faae1a2bda93dff32e9135e88281b3255115c4d
Francesco D'Alessandro <dalefra0@gmail.com>
2025-12-07 17:51:57 +0100

    rimozione di composer

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,12 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+// inclusione manuale dei file PHP
+require __DIR__ . '/../src/Database/Connection.php';
+require __DIR__ . '/../src/Models/User.php';
+require __DIR__ . '/../src/Controllers/QuizController.php';
 
+// usa i namespace
 use SiParte\Quiz\Controllers\QuizController;
 
+// demo quiz
 $quiz = new QuizController();
 echo $quiz->demoQuiz();
